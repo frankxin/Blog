9dcecf0bf5d414b1e413582c0824f87d4d100a26
frankxin <frankxin93@gmail.com>
2019-05-05 17:17:46 +0800

    [update] 新增anchor功能

--- frontend/Article/index.tsx
+++ frontend/Article/index.tsx
@@ -18,7 +18,8 @@ interface ArticleState {
         title: string,
         date: string,
         tag: string
-    }
+    },
+    intialRender: boolean
 }
 export class Article extends React.Component<ArticleProps, ArticleState> {
     constructor(props: ArticleProps) {
@@ -30,7 +31,8 @@ export class Article extends React.Component<ArticleProps, ArticleState> {
                 title: '',
                 date: '',
                 tag: ''
-            }
+            },
+            intialRender: true
         }
     }
     async componentDidMount() {
@@ -67,16 +69,32 @@ export class Article extends React.Component<ArticleProps, ArticleState> {
             + (escaped ? code : _.escape(code))
             + '</code></pre>\n';
         };
+        renderer.heading = (text, level, raw, slugger) => {
+            return '<h'
+                + level
+                + ' id="'
+                + slugger.slug(raw)
+                + '">'
+                + `<a class="anchor" name="${text}" href="`+ `#${text}` + '"><svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg></a>'
+                + text
+                + '</h'
+                + level
+                + '>\n';
+        }
         
         this.setState({
             content: marked(res.data.content, { renderer }),
             metaData: res.data.metaData
         })
-
+        
         loadDisqus(this.props.match.params.id, res.data.metaData.title)
     }
 
     componentDidUpdate(prevProps: ArticleProps, prevState: ArticleState) {
+        if(this.state.intialRender){
+            jump(location.hash)
+            this.setState({intialRender: false})
+        }
         if (this.props.match.params.id !== prevProps.match.params.id) {
             http.get(`/v1/articles/${this.props.match.params.id}`)
                 .then(res => {
@@ -116,4 +134,10 @@ function loadDisqus(id: string, title: string): void {
     s.src = 'https://frankxin93.disqus.com/embed.js';
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
+}
+
+function jump(h){
+    var url = location.href;               //Save down the URL without hash.
+    location.href = h;                 //Go to the target element.
+    history.replaceState(null,null,url);   //Don't like hashes. Changing it back.
 }
